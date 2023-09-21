63dfe43d1989df90665f61bc29cd737222a4dc77
sophiee10 <145008196+sophiee10@users.noreply.github.com>
2023-09-21 12:29:02 +0200

    Add files via upload

---
+++ Neuer Ordner/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  
+  background(120,100,240);
+  fill(50,200,200);
+  stroke(30,10,150);
+  strokeWeight(5);
+  ellipse(200,150,110,100);
+  stroke(30,10,150)
+  line(200,200,200,300)
+  line(120,190,200,220)
+  line(200,220,280,180)
+  line(150,350,200,300)
+  line(200,300,250,350)
+  
+}
